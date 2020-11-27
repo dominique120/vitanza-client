2d04ee4c037293e016ab007446a4b57a90f5fa8b
Miluska <Milumon@outlook.com>
2020-11-27 02:34:18 -0500

    web POST

--- VitanzaWeb/assets/js/app.js
+++ VitanzaWeb/assets/js/app.js
@@ -1,41 +1,41 @@
- let Auth_Bearer = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE2MDY0NTgzNDksImlhdCI6MTYwNjQyMjM0OSwiaXNzIjoidnRzLXRlc3RpbmciLCJwYXNzd29yZCI6InBhc3N3b3JkIiwidXNlcm5hbWUiOiJ1c2VybmFtZSJ9.c5MUzz3-BI2oLrKCsjP6tl5flxRba74l1OG3V9rAbYrBrOawP3o9PWq7MpxE6QLfFLPG19zlckx997Jv8TkMTA";
+ let Auth_Bearer = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXUyJ9.eyJleHAiOjE2MDY0OTQ1MTUsImlhdCI6MTYwNjQ1ODUxNSwiaXNzIjoidnRzLXRlc3RpbmciLCJwYXNzd29yZCI6InBhc3N3b3JkIiwidXNlcm5hbWUiOiJ1c2VybmFtZSJ9.wB4fmWOrN4nBAD8kFptW_GQtFvy7cCyjHhDC_CfZKqq33iwS92y6b-auNnsXDsyH86FqdkFepmqilrEcVSJ13g";
  const proxyurl = "https://cors-anywhere.herokuapp.com/";
  const urlCustomers = "http://vts-alb-316342429.us-east-1.elb.amazonaws.com/vts/api/v1/customers";
-
+ 
 
  function traer() {
 
+   
 
-
-   document.getElementById("tabla-listar").style.display = "block";
-   document.getElementById("tabla-crear").style.display = "none";
-
-
-   fetch(proxyurl + urlCustomers, {
-       method: 'GET',
-       dataType: "jsonp",
-       crossDomain: true,
-       cors: true,
-       headers: {
-         "Content-Type": "application/json",
-         "Access-Control-Allow-Origin": "*",
-         "Authorization": "Bearer " + Auth_Bearer,
-       }
-     })
-     .then(response => response.json())
-     .then(clientes => {
-       tabla(clientes)
-     });
+  document.getElementById("tabla-listar").style.display = "block";
+  document.getElementById("tabla-crear").style.display = "none";
 
  }
  // LISTAR
+ 
+ fetch(proxyurl + urlCustomers, {
+  method: 'GET',
+  dataType: "jsonp",
+  crossDomain: true,
+  cors: true,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Authorization": "Bearer " + Auth_Bearer,
+  }
+})
+.then(response => response.json())
+.then(clientes => {
+  tabla(clientes)
+  
+});
 
 
  function tabla(clientes) {
-
+   
    var contenido = document.querySelector('#contenido');
    console.log(clientes);
-   for (let cliente of clientes) {
+   for (let cliente of clientes) { 
      contenido.innerHTML += `
 
                    <tr>
@@ -52,91 +52,68 @@
                     `
    }
  }
-
  
+// CREAR
+
+var form = document.getElementById('form')
+
+form.addEventListener('submit', function(e){
+
+// auto submision of the form
+
+e.preventDefault()
+
+var FirstName = document.getElementById('FirstName').value
+var LastNames = document.getElementById('LastNames').value
+var District = document.getElementById('District').value
+var PrimaryAddress = document.getElementById('PrimaryAddress').value
+var PrimaryPhone = document.getElementById('PrimaryPhone').value
 
 
- $(document).on("click", "#btn-crear", function (event) {
-      
- console.log(formData);
+fetch(proxyurl + urlCustomers, {
+  method: 'POST', 
+  body: JSON.stringify({
+    FirstName: FirstName,
+    LastNames: LastNames,
+    District: District,
+    PrimaryAddress: PrimaryAddress,
+    PrimaryPhone: PrimaryPhone, 
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8', 
+    'Access-Control-Allow-Origin': '*',
+    'Authorization': "Bearer " + Auth_Bearer,
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json))
 
-       var firstNames = $("#FirstNames").val();
-       var lastNames = $("#LastNames").val();
-       var district = $("#District").val();
-       var primaryAddress = $("#PrimaryAddress").val();
-       var primaryPhone = $("#PrimaryPhone").val();
-   
+})
+  
  
+  
 
-       var ruta = "http://190.238.243.118/vts/api/v1/customersf";
+ function crearForm() { 
 
-       var formData = new FormData();
+   document.getElementById("tabla-listar").style.display = "none";
+   document.getElementById("tabla-crear").style.display = "block";
 
-       formData.append("FirstNames", firstNames);
-       formData.append("LastNames", lastNames);
-       formData.append("District", district);
-       formData.append("PrimaryAddress", primaryAddress);
-       formData.append("PrimaryPhone", primaryPhone);
+ };
 
-
-
-       fetch(ruta, {
-
-         method: 'POST',
-
-         body: formData
-
-       })
-      });
-
-       /*
-       function submitform(){
-
-         console.log(formData);
+ // DELETE
 
 
-         $.ajax({
-           type: "POST",
-           url: "http://190.238.243.118/vts/api/v1/customersf", 
-           data: formData,
-           success: function(){},
-           dataType: "json",
-           headers: {
-             "Content-Type": "application/json",
-             "Access-Control-Allow-Origin": "*",
-             "Authorization": "Bearer " + Auth_Bearer,
-           },
-           
-           contentType : "application/json"
-         });
+ $(document).on("click", ".btn-delete", function (event) {
+
+   var _this = $(this).attr("data-id");
+   console.log("id: ", _this);
+
+   const response = fetch(
+     "https://cors-anywhere.herokuapp.com/" + urlCustomers + "?id=" + _this, {
+       method: 'DELETE',
+       headers: {
+         "Access-Control-Allow-Origin": "*",
+         "Authorization": "Bearer " + Auth_Bearer,
        }
-
-        */
-
-       function crearForm() {
-
-
-
-
-         document.getElementById("tabla-listar").style.display = "none";
-         document.getElementById("tabla-crear").style.display = "block";
-
-       };
-
-       // DELETE
-
-
-       $(document).on("click", ".btn-delete", function (event) {
-
-         var _this = $(this).attr("data-id");
-         console.log("id: ", _this);
-
-         const response = fetch(
-           "https://cors-anywhere.herokuapp.com/" + urlCustomers + "?id=" + _this, {
-             method: 'DELETE',
-             headers: {
-               "Access-Control-Allow-Origin": "*",
-               "Authorization": "Bearer " + Auth_Bearer,
-             }
-           });
-       });
+     });
+ });
